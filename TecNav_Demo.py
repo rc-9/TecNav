c6698294342151204b4f6971a02329eef69497f6
RC <82172988+rc-9@users.noreply.github.com>
2022-05-28 14:07:27 -0500

    Edited notebooks

--- TecNav_Demo.py
+++ TecNav_Demo.py
@@ -18,10 +18,15 @@ from io import StringIO
 # TecNav navigator
 from uc_navigator import *
 
+# Utils
+from datetime import datetime, timedelta, date
+from random import shuffle
+
 # Read in necessary data files
 past_patients_df = pd.read_pickle('./pickled_objects/past_patients_df.pkl')
 new_patients_df = pd.read_pickle('./pickled_objects/new_patients_df.pkl')
-clinics_df = pd.read_csv('./uc_clinics.csv', index_col='branch_name')
+
+clinics_df = pd.read_csv('./fabricated_data/uc_clinics.csv', index_col='branch_name')
 clinics_df['nearby_clinics'] = clinics_df.nearby_clinics.apply(lambda x: ast.literal_eval(x))
 model = pickle.load(open('./pickled_objects/rf_model.pkl', 'rb'))
 
@@ -32,7 +37,11 @@ c3.title("""TecNav""")
 
 # Setup sidebar with date toggle-menu
 st.sidebar.header('Simulate a past day: ')
-selected_date = st.sidebar.selectbox('Date: ', past_patients_df.visit_date.unique())
+unique_dates = list(past_patients_df.visit_date.unique())
+selected_date = st.sidebar.selectbox('Date: ', unique_dates)
+st.sidebar.header('Examine movement: ')
+selected_loc = st.sidebar.selectbox('Location: ', ['Denver', 'Edgewater', 'Wheatridge', 'Rino', 'Lakewood'])
+
 st.markdown(f'''
     <style>
     section[data-testid="stSidebar"] .css-ng1t4o {{width: 14rem;}}
@@ -42,6 +51,7 @@ st.markdown(f'''
 # Filter data based on selected date
 df = past_patients_df.copy()
 df = df[df.visit_date == selected_date]
+df['new_num_techs'] = df['assigned_num_techs'] - 1
 
 # Ensure std output streams to dashboard
 @contextmanager
@@ -55,7 +65,7 @@ def st_capture(output_func):
         stdout.write = new_write
         yield
 
-# Set up separate columns for visuals and std out        
+# Set up separate columns for visuals and std out
 col1, col2 = st.columns((1, 1.25))
 
 # Stream stdout to right column
@@ -67,6 +77,50 @@ with st_capture(output.code):
     df['current_num_techs'] = nav.current_num_techs_col
     df.to_csv('./uc_log.csv')
 
+### GEO PLOT
+def floor_dt(current_time):
+    today = date.today()
+    current_time = datetime.combine(today, current_time)
+    rounded_time = current_time - (current_time - datetime.min) % timedelta(minutes=15)
+    return str(rounded_time.time())[:-3]
+df['interval_time'] = df.checkin_time.apply(lambda x: floor_dt(x))
+all_intervals = df.interval_time.unique()
+rows = []
+for i in all_intervals:
+    rows.append(['denver', i])
+    rows.append(['wheatridge', i])
+    rows.append(['edgewater', i])
+    rows.append(['rino', i])
+    rows.append(['lakewood', i])
+new_df = pd.DataFrame(rows, columns=['visit_location', 'time'])
+new_df['lat'] = new_df.visit_location.map(clinics_df.to_dict()['lat'])
+new_df['lon'] = new_df.visit_location.map(clinics_df.to_dict()['lon'])
+new_df = new_df.sort_values(by=['visit_location', 'time'])
+new_df['rolling_ct'] = new_df[['visit_location', 'time']].apply(lambda x: df[(df.visit_location == x[0]) & (df.interval_time == x[1])]['rolling_ct'].tolist(), axis=1)
+new_df['rolling_ct'] = new_df['rolling_ct'].apply(lambda x: round(np.mean(x)) if x != [] else np.nan)
+new_df['rolling_ct'] = new_df['rolling_ct'].fillna(method='ffill')
+new_df['needed_num_techs'] = new_df[['visit_location', 'time']].apply(lambda x: df[(df.visit_location == x[0]) & (df.interval_time == x[1])]['needed_num_techs'].tolist(), axis=1)
+new_df['needed_num_techs'] = new_df['needed_num_techs'].apply(lambda x: round(np.mean(x)) if x != [] else np.nan)
+new_df['needed_num_techs'] = new_df['needed_num_techs'].fillna(method='ffill')
+new_df = new_df.fillna(0)
+col1.subheader(f'Rolling Patient & Tech Count on {selected_date}:')
+fig = px.scatter_mapbox(
+    new_df,
+    lat="lat",
+    lon="lon",
+    size="rolling_ct",
+    color='needed_num_techs',
+    hover_data=["needed_num_techs"],
+    animation_frame="time",
+    size_max=50,
+    color_continuous_scale='YlOrRd', #'YlOrRd',
+    # title=f'Rolling Patient Count on {selected_date}'
+).update_layout(height=400, width=600,
+    mapbox={"style": "carto-positron", "zoom":10.5}, margin={"l": 0, "r": 0, "t": 0, "b": 0})
+
+col1.plotly_chart(fig, use_container_width=True)
+
+
 
 # Retreive necessary logged data based on navigation execution
 results = pd.read_csv('uc_log.csv')
@@ -77,27 +131,26 @@ edgewater_df = results[results.visit_location == 'edgewater']
 rino_df = results[results.visit_location == 'rino']
 lakewood_df = results[results.visit_location == 'lakewood']
 
-col1.subheader(f'Technician Count on {selected_date}:')
+# col1.subheader(f'Technician Count on {selected_date}:')
 
-### PLOTLY PLOTS
+
+### TECH COUNT PLOTS
 dfs = [denver_df, wheatridge_df, edgewater_df, rino_df, lakewood_df]
-locs = ['Denver', 'Wheatridge', 'Edgewater', 'RiNo', 'Lakewood']
-for i in range(5):
-    col1.write(f'{locs[i]}:')
-    
+locs = ['Denver', 'Wheatridge', 'Edgewater', 'RiNo (River-North District)', 'Lakewood']
+
+def plot_tech_needs(loc, loc_df):
     fig = go.Figure()
-    loc_df = dfs[i]
 
     line1 = go.Scatter(
-        x=loc_df['checkin_time'], y=loc_df['assigned_num_techs'], 
-        name='Scheduled', marker_color='yellow', line = dict(color='yellow', width=4, dash='dash'), mode='lines'
+        x=loc_df['checkin_time'], y=loc_df['assigned_num_techs'],
+        name='Before', marker_color='yellow', line = dict(color='yellow', width=4, dash='dash'), mode='lines'
     )
     line2 = go.Scatter(
-        x=loc_df['checkin_time'], y=loc_df['current_num_techs'], 
+        x=loc_df['checkin_time'], y=loc_df['current_num_techs'],
         name='Current', marker_color='#17becf', mode='lines'
     )
     line3 = go.Scatter(
-        x=loc_df['checkin_time'], y=loc_df['needed_num_techs'], 
+        x=loc_df['checkin_time'], y=loc_df['needed_num_techs'],
         name='Needed', marker_color='red', mode='lines'
     )
 
@@ -106,11 +159,25 @@ for i in range(5):
     fig.add_trace(line2)
     fig.add_trace(line3)
     fig.update_traces(opacity=0.7)
-    fig.update_layout(height=300, width=1200, showlegend=True, legend_tracegroupgap=250)
+    fig.update_layout(height=350, width=1200, showlegend=True, legend_tracegroupgap=250)
     fig.update_xaxes(showgrid=False, tickangle=-45)
     fig.update_yaxes(showgrid=False)
-    
+    fig.update_layout(
+    title={
+        'text': loc,
+        # 'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
+
     col1.plotly_chart(fig, use_container_width=True)
+
+plot_specs = {'Denver': ['Denver', denver_df],
+              'Wheatridge':['Wheatridge', wheatridge_df],
+              'Edgewater':['Edgewater', edgewater_df],
+              'Rino':['RiNo (River-North Art District)', rino_df],
+              'Lakewood':['Lakewood', lakewood_df]}
+plot_tech_needs(plot_specs[selected_loc][0], plot_specs[selected_loc][1])
 
 
 #######################################
--- uc_navigator.py
+++ uc_navigator.py
@@ -4,7 +4,7 @@
 ##     _______________    _______________    _______________    ____          __     ____     __            __      ##
 ##    _______________    _______________    _______________    / /\ \        / /    / /\ \    \ \          / /      ##
 ##         ___          / /                / /                / /  \ \      / /    / /  \ \    \ \        / /       ##
-##        ___          / /========        / /                / /    \ \    / /    / / == \ \    \ \      / /        ##
+##        ___          / /________        / /                / /    \ \    / /    / / __ \ \    \ \      / /        ##
 ##       ___          / /                / /                / /      \ \  / /    / /      \ \    \ \    / /         ##
 ##      ___          _______________    _______________    / /        \ \/ /    / /        \ \    \ \__/ /          ##
 ##     ___          _______________    _______________    / /          \ \/    / /          \ \    \____/           ##
@@ -29,22 +29,38 @@ from time import sleep
 
 class TecNav:
 
-    def __init__(self, df, clinics_info, model):
+    def __init__(self, df, clinics_info, model, code_ratio=0):
         """Instantiate necessary data structures required for application."""
 
         self.df = df
         self.clinics_info = clinics_info
         self.model = model
+        if code_ratio == None:
+            self.code_ratio = 0
+        else:
+            self.code_ratio = code_ratio
 
         self.movements = []
         self.current_num_techs_col = []
+        self.moves_within_hour = 0
+        self.stag = 0
+        self.potential_move = 0
+
+        # Scheduled starting technician count at each location
+        denver_start = df[df.visit_location == 'denver'].iloc[0]['new_num_techs']
+        wheatridge_start = df[df.visit_location == 'wheatridge'].iloc[0]['new_num_techs']
+        edgewater_start = df[df.visit_location == 'edgewater'].iloc[0]['new_num_techs']
+        rino_start = df[df.visit_location == 'rino'].iloc[0]['new_num_techs']
+        lakewood_start = df[df.visit_location == 'lakewood'].iloc[0]['new_num_techs']
+
         self.tracker = {
-            'denver': {'checkin_time': None, 'num_techs': 0,'needed_techs': 0, 'flag': 0, 'available_techs': 0, 'num_movements': 0},
-            'edgewater': {'checkin_time': None, 'num_techs': 0,'needed_techs': 0, 'flag': 0, 'available_techs': 0, 'num_movements': 0},
-            'wheatridge': {'checkin_time': None, 'num_techs': 0,'needed_techs': 0, 'flag': 0, 'available_techs': 0, 'num_movements': 0},
-            'rino': {'checkin_time': None, 'num_techs': 0,'needed_techs': 0, 'flag': 0, 'available_techs': 0, 'num_movements': 0},
-            'lakewood': {'checkin_time': None, 'num_techs': 0,'needed_techs': 0, 'flag': 0, 'available_techs': 0, 'num_movements': 0}
+            'denver': {'checkin_time': None, 'num_techs': denver_start,'needed_techs': 0, 'flag': 0, 'available_techs': 0, 'rolling_code': 0, 'timestamp_recent_move': None},
+            'edgewater': {'checkin_time': None, 'num_techs': edgewater_start,'needed_techs': 0, 'flag': 0, 'available_techs': 0, 'rolling_code': 0, 'timestamp_recent_move': None},
+            'wheatridge': {'checkin_time': None, 'num_techs': wheatridge_start,'needed_techs': 0, 'flag': 0, 'available_techs': 0, 'rolling_code': 0, 'timestamp_recent_move': None},
+            'rino': {'checkin_time': None, 'num_techs': rino_start,'needed_techs': 0, 'flag': 0, 'available_techs': 0, 'rolling_code': 0, 'timestamp_recent_move': None},
+            'lakewood': {'checkin_time': None, 'num_techs': lakewood_start,'needed_techs': 0, 'flag': 0, 'available_techs': 0, 'rolling_code': 0, 'timestamp_recent_move': None}
             }
+
 
     def update_tracker(self, indx, row):
         """Update tracker dictionary with current iteration's patient check-in record information."""
@@ -53,17 +69,18 @@ class TecNav:
         self.indx = indx
         self.location = row['visit_location']
         self.checkin_time = row['checkin_time']
-        self.num_techs = row['new_num_techs']
+        self.num_techs = self.tracker[self.location]['num_techs']
         self.needed_techs = row['needed_num_techs']
 
         # Update dynamic dictionary (tracker) with current record's data
         self.tracker[self.location]['checkin_time'] = self.checkin_time
-        self.current_num_techs = self.num_techs + self.tracker[self.location]['num_movements']
+        self.current_num_techs = self.tracker[self.location]['num_techs']
         self.current_num_techs_col.append(self.current_num_techs)
-        self.tracker[self.location]['num_techs'] = self.current_num_techs
-        self.tracker[self.location]['needed_techs'] = self.needed_techs + self.tracker[self.location]['num_movements']
+        self.tracker[self.location]['needed_techs'] = self.needed_techs
         self.available_techs = self.tracker[self.location]['num_techs'] - self.tracker[self.location]['needed_techs']
         self.tracker[self.location]['available_techs'] = self.available_techs
+        self.tracker[self.location]['rolling_code'] = row['rolling_code']
+
         return
 
     def check_flag_is_one(self):
@@ -89,7 +106,10 @@ class TecNav:
     def do_we_navigate(self):
         """Called upon if flag was already marked, to check if more technicians are needed."""
 
-        return self.tracker[self.location]['num_techs'] < self.tracker[self.location]['needed_techs']
+        bool = (self.tracker[self.location]['num_techs'] < self.tracker[self.location]['needed_techs'])  \
+            & (self.tracker[self.location]['rolling_code'] >= self.code_ratio)
+
+        return bool
 
     def check_availability(self):
         """Called upon if we should navigate, to check the availability of nearby clinics."""
@@ -125,7 +145,6 @@ class TecNav:
                 elif needed_num_techs_pred >= self.tracker[pull_from]['needed_techs']:
                     print(f"        - ML model recommends no transfer from {pull_from.capitalize()}")
                     available.pop(0)
-
         return None
 
     def pull_technician(self, pull_from):
@@ -134,18 +153,26 @@ class TecNav:
 
         departure_time = datetime.strptime(str(self.checkin_time), '%H:%M:%S') + timedelta(minutes=2, seconds=choice([i for i in range(60)]))
 
+        # EVALUATION PURPOSES: Track instances where clinic who transferred a tech, needed one within next hour
+        if self.tracker[self.location]['timestamp_recent_move'] != None:
+            if departure_time + timedelta(minutes=60) < self.tracker[self.location]['timestamp_recent_move']:
+                self.moves_within_hour += 1
+
+        self.tracker[pull_from]['timestamp_recent_move'] = departure_time
+
         print(f'- Technician from {pull_from.capitalize()} left at {departure_time.time()}')
 
         # Update tracker dict by subtracting 1 tech from pull_from
-        self.tracker[pull_from]['num_movements'] = -1
+        self.tracker[pull_from]['num_techs'] += -1
+
         # Update tracker dict by adding 1 to location
-        self.tracker[self.location]['num_movements'] = 1
+        self.tracker[self.location]['num_techs'] += 1
 
         travel_time = int(self.clinics_info.loc[pull_from, 'to_'+self.location]) + choice([0, 2, 3, 4, 5, 6])
         arrival_time = departure_time + timedelta(minutes=int(travel_time), seconds=choice([i for i in range(60)]))
         print(f"- Technician from {pull_from.capitalize()} arrived at {self.location.capitalize()} at {arrival_time.time()}")
-        print(f'- {self.location.capitalize()}: before count = {self.num_techs} | after count = {self.num_techs + self.tracker[self.location]["num_movements"]}')
-        print(f'- {pull_from.capitalize()}: before count = {self.tracker[pull_from]["num_techs"]} | after count = {self.tracker[pull_from]["num_techs"] + self.tracker[pull_from]["num_movements"]}')
+        print(f'- {self.location.capitalize()}: before count = {self.num_techs} | after count = {self.tracker[self.location]["num_techs"]}')
+        print(f'- {pull_from.capitalize()}: before count = {self.tracker[pull_from]["num_techs"]+1} | after count = {self.tracker[pull_from]["num_techs"]}')
         print()
         self.movements.append((pull_from, self.location))
         return
@@ -172,6 +199,7 @@ class TecNav:
                 # If location was previously flagged, check if navigation should be attempted
                 if self.do_we_navigate() == True:
                     print(f"{self.checkin_time} - {self.location.capitalize()} Clinic needs a technician")
+                    self.potential_move += 1
 
                     # Check which clinics have available technicians
                     available = self.check_availability()
@@ -182,6 +210,7 @@ class TecNav:
 
                     # If there is no location where its feasible to pull an available technician from, print notice
                     if pull_from == None:
+                        self.stag += 1
                         print('No movement at this time; all clinics are busy.\n')
                         # track the count of how many times this occurs??
 
@@ -201,24 +230,18 @@ def main():
     # Read in necessary data files and pre-constructed model object
     past_patients_df = pd.read_pickle('./pickled_objects/past_patients_df.pkl')
     new_patients_df = pd.read_pickle('./pickled_objects/new_patients_df.pkl')
-    clinics_df = pd.read_csv('./uc_clinics.csv', index_col='branch_name')
+    clinics_df = pd.read_csv('./fabricated_data/uc_clinics.csv', index_col='branch_name')
     clinics_df['nearby_clinics'] = clinics_df.nearby_clinics.apply(lambda x: ast.literal_eval(x))
     model = pickle.load(open('./pickled_objects/rf_model.pkl', 'rb'))
-
-    # Instantiate logger for application
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    fh = logging.FileHandler('uc_navigation.log', 'w')
-    fh.setLevel(logging.DEBUG)
-    # logger.addHandler(fh)
-    sh = logging.StreamHandler()
-    sh.setLevel(logging.INFO)
-    logger.addHandler(sh)
 
     # Setup parser for customized execution based on CLI arguments
     parser = argparse.ArgumentParser(description='Specify preferences for TecNav execution.')
     parser.add_argument('data_src', metavar='<source>', type=str, choices=['past', 'new'], help='Choose to execute TecNav with streamed data from "past" or "new" patient records.')
     parser.add_argument('-d', '--date', dest='specified_date', metavar='<date>', type=str, help='Specify a date of interest to stream patient records (OPTIONAL).')
+    parser.add_argument('-s', '--starting', dest='specified_starting_ct', metavar='<start>', type=int, help='Specify a modified starting number of assigned technicians (OPTIONAL).')
+    parser.add_argument('-r', '--ratio', dest='specified_ratio', metavar='<ratio>', type=int, help='Specify a threshold ratio of number of patients per technician (OPTIONAL).')
+    parser.add_argument('-c', '--code', dest='specified_code', metavar='<code>', type=float, help='Specify a threshold average code to trigger moves (OPTIONAL).')
+
     args = parser.parse_args()
 
     # Conduct modifications based on user-provided specifications
@@ -229,8 +252,25 @@ def main():
     if args.specified_date:
         df = df[df.visit_date.astype(str) == args.specified_date]
 
+    staff_ratio = args.specified_ratio
+    code_ratio = args.specified_code
+    start_ct_modification = args.specified_starting_ct
+    if staff_ratio != None:
+        aggregated_df = df.groupby(['visit_date', 'visit_location']).max()[['rolling_ct']].reset_index(drop=False)
+        aggregated_df['assigned_num_techs'] = aggregated_df['rolling_ct'].apply(lambda x: int(x/staff_ratio)+1 if x%staff_ratio != 0 else int(x/staff_ratio))
+        schedule_zipper = zip(aggregated_df.visit_date, aggregated_df.visit_location, aggregated_df.assigned_num_techs)
+        schedule_dict = {}
+        for i in schedule_zipper:
+            schedule_dict[(i[0], i[1])] = i[2]
+        df['assigned_num_techs'] = df[['visit_date', 'visit_location']] \
+            .apply(lambda x: (x[0], x[1]), axis=1) \
+            .map(schedule_dict)
+
+    if start_ct_modification != None:
+        df['new_num_techs'] = df['assigned_num_techs'] + start_ct_modification
+
     # Instantiate navigator application class & feed in the necessary data
-    nav = TecNav(df, clinics_df, model)
+    nav = TecNav(df, clinics_df, model, code_ratio)
     nav.execute_navigation()
     df['current_num_techs'] = nav.current_num_techs_col
     df.to_csv('./uc_log.csv')
